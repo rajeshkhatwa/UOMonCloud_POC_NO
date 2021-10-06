f81eb3ee7dfafa5da026503194cc7a2285b68908
Rajesh Kumar Khatwa <rajesh.kumar.khatwa@sap.com>
2021-10-06 05:20:51 +0000

    Custom Handler Updated

--- srv/src/main/java/customer/networkobjects/handlers/NetworkObjectServiceHandler.java
+++ srv/src/main/java/customer/networkobjects/handlers/NetworkObjectServiceHandler.java
@@ -3,8 +3,12 @@ package customer.networkobjects.handlers;
 import java.util.List;
 import java.util.stream.Stream;
 
+import com.sap.cds.Result;
 import com.sap.cds.ql.Select;
+import com.sap.cds.ql.Update;
 import com.sap.cds.ql.cqn.CqnSelect;
+import com.sap.cds.ql.cqn.CqnSelectListItem;
+import com.sap.cds.ql.cqn.CqnUpdate;
 import com.sap.cds.services.cds.CdsReadEventContext;
 import com.sap.cds.services.cds.CqnService;
 import com.sap.cds.services.handler.EventHandler;
@@ -12,11 +16,17 @@ import com.sap.cds.services.handler.annotations.After;
 import com.sap.cds.services.handler.annotations.Before;
 import com.sap.cds.services.handler.annotations.On;
 import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.persistence.PersistenceService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import cds.gen.networkobjects.db.NetworkObjectTypes;
+import cds.gen.networkobjects.db.NetworkObjectTypes_;
 import cds.gen.networkobjectsservice.NetworkObjects;
 import cds.gen.networkobjectsservice.NetworkObjectsService_;
+
+
 
 
 @Component
@@ -24,14 +34,50 @@ import cds.gen.networkobjectsservice.NetworkObjectsService_;
 public class NetworkObjectServiceHandler implements EventHandler
 {
  
-    @After(event = CqnService.EVENT_READ)
+    @Autowired
+    PersistenceService db;
+
+    @After(event = CqnService.EVENT_READ , entity = "NetworkObjectsService.NetworkObjects")
     public void afterReadNetObj( Stream<NetworkObjects> NetObj )
     {
+      String Description;
       System.out.println("Inside the after  Read Method" );
       NetObj.peek(b -> System.out.println("Original Value : "  +  b.getNetworkObjectType() )  )
       .filter(b -> b.getNetworkObjectType().equals("CLUSTER") )
       .peek(b -> System.out.println("Filtered Value before change description : "  +  b.getNetworkObjectDescription() )  )
       .forEach(b -> b.setNetworkObjectDescription("Cluster_Changed_Value"));
+      
+     CqnSelect select = Select.from(NetworkObjectTypes_.class).columns(b -> b.network_object_type( ),
+                                                                       b -> b.network_object_type_description( ) );
+
+     Result NObjTypes = db.run(select);
+     //instead of List of ..you can use Stream of also.
+     Description = NObjTypes.listOf(NetworkObjectTypes.class).get(0).getNetworkObjectTypeDescription();
+     System.out.println(Description);
+
+
+     List<NetworkObjectTypes> NOList = NObjTypes.listOf(NetworkObjectTypes.class);
+
+    // Update the Network Object Type Description
+    NObjTypes.listOf(NetworkObjectTypes.class).get(0).setNetworkObjectTypeDescription("Changed using Code : CQN Update");
+    CqnUpdate update = Update.entity(NetworkObjectTypes_.class).data("network_object_type_description","Changed using Code : CQN Update" )
+                       .where(b -> b.network_object_type().eq("CLUSTER") );
+
+          db.run(update);          
+    // 
+
+    }
+
+    @Before(event = CqnService.EVENT_READ , entity = "NetworkObjectsService.NetworkObjects")
+    public void beforeReadNetObj( CdsReadEventContext ctx )
+    {
+        // Get List of Columns
+        Select <?> select = Select.copy(ctx.getCqn());
+        //this is deperecated , should use items method now.
+
+        // Can use setCqn to modify the comuns to be selected
+        List<CqnSelectListItem> columns = select.columns();
+        List<CqnSelectListItem> columns2 = select.items();
       
     
     }
